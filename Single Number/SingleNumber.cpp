12a7b41ca9584a88889df940a60a3b2face6e9c0
Bhaskar Singh <rougesocket@gmail.com>
2022-06-09 16:36:46 +0530

    Single Number Completed

---
+++ Single Number/SingleNumber.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int n= nums.size(),ans=nums[0];
+        //iterating and find xor as per the approach in the notes
+		for(int i=1;i<n;i++)ans^=nums[i];
+        return ans;
+    }
+};
