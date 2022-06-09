2fef523df35283bf6440e0516bef14859866fa90
rougesocket <78953377+rougesocket@users.noreply.github.com>
2022-06-09 16:38:19 +0530

    Update SingleNumber.cpp

--- Single Number/SingleNumber.cpp
+++ Single Number/SingleNumber.cpp
@@ -4,7 +4,7 @@ public:
         
         int n= nums.size(),ans=nums[0];
         //iterating and find xor as per the approach in the notes
-		for(int i=1;i<n;i++)ans^=nums[i];
+	for(int i=1;i<n;i++)ans^=nums[i];
         return ans;
     }
 };
